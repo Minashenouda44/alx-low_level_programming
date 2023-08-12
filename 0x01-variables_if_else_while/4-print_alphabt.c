959d4113126fbbd342781752f9401a35792c893e
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-12 00:03:18 +0000

    for if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char L;
+
+	for (L = 'a' ; L <= 'z' ; L++)
+	{
+		if (L != 'e' && L != 'q')
+			putchar(L);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
