39fa9fd03ebbd7c6c53bb032752c5e9b63e668cb
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-12 01:08:03 +0000

    putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s;
+
+	for (s = '0' ; s <= '9' ; s++)
+	{
+		putchar(s);
+
+		if (s != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
