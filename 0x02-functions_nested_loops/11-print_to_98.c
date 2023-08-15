93f2290274ef1200bffaa487392e54d9ecd4bb2f
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-15 22:59:27 +0000

    x

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,6 @@
  *
  * Return: void
  */
-
 void print_to_98(int n)
 {
 	int i;
@@ -17,20 +16,25 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+
+			if (i != 98)
+				_putchar(',');
+				_putchar(' ');
 		}
 	}
 	else
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+
+			if (i != 98)
+				_putchar(',');
+				_putchar(' ');
 		}
 	}
-
 	_putchar('\n');
 }
