e63ef118e6c2d95b6477c923c5a89686bd0493a1
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-16 00:53:02 +0000

    a

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -18,12 +18,13 @@ int main(void)
 			s += i;
 	}
 
+	_putchar((s / 1000000) % 10 + '0');
 	_putchar((s / 100000) % 10 + '0');
 	_putchar((s / 10000) % 10 + '0');
 	_putchar((s / 1000) % 10 + '0');
 	_putchar((s / 100) % 10 + '0');
 	_putchar((s / 10) % 10 + '0');
-	_putchar(s % 10 + '0');
+	_putchar((s / 1) % 10 + '0');
 
 	return (0);
 }
