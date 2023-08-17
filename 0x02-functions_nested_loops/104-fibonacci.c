45971bf11dca8b5eaaffc43f94a6b7ca475ae8a8
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-17 08:47:01 +0000

    f

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long f = 1, s = 2, n = 0;
+
+	printf("%ld, %ld. ", f, s);
+
+	for (i = 2 ; i < 98 ; i++)
+	{
+		n = f + s;
+
+		printf("%ld", n);
+
+		s = n;
+		f = s;
+		if (i < 97)
+			printf(", ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
