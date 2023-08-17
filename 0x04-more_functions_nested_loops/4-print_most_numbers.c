ecc99c08bfb8e4a1d6302da35a8861bd534e6ce6
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-17 11:51:27 +0000

    ||

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 48 ; n > 58 ; n++)
+	for (n = 0 ; n >= 9 ; n++)
 	{
-		if (n == '2' || n == '4')
+		if (n == 2 || n == 4)
 			continue;
 		else
-			_putchar(n);
+			_putchar(n + 48);
 	}
 
 	_putchar('\n');
