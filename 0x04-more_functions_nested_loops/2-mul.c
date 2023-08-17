d3260ce35a29d6b963135b9053da97303d6599e6
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-17 11:31:28 +0000

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ * @a : 1st num
+ * @b : 2nd num
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48 ; n > 58 ; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n);
+	}
+
+	_putchar('\n');
+}
