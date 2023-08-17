fd4790f1819d146257064e5553cb08b09ed5ed3a
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-17 08:30:45 +0000

    while

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,18 +10,21 @@ int main(void)
 {
 	unsigned long f = 0, s = 1, n, es;
 
-	while (es <= 4000000)
+	while (1)
 	{
 		n = f + s;
 		f = s;
 		s = n;
+
+		if (es > 4000000)
+			break;
 
 		if (n % 2 == 0)
 			es += n;
 
 	}
 
-	printf("%ld", es);
+	printf("%ld\n", es);
 
 	return (0);
 }
