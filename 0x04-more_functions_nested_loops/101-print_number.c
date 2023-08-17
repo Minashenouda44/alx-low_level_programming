ad0dded119038d8add77d976ed209ec4cf492a44
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-17 15:38:55 +0000

    i

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,22 +8,24 @@
 
 void print_number(int n)
 {
-	if (n <= 9 && n >= -9)
+	if (n == 0)
+		_putchar(n);
+	else if (n <= 9 || n >= -9)
 	{
 		_putchar((n / 1) % 10 + '0');
 	}
-	else if (n <= 99 && n >= -99)
+	else if (n <= 99 || n >= -99)
 	{
 		_putchar((n / 10) % 10 + '0');
 		_putchar((n / 1) % 10 + '0');
 	}
-	else if (n <= 999 && n >= -999)
+	else if (n <= 999 || n >= -999)
 	{
 		_putchar((n / 100) % 10 + '0');
 		_putchar((n / 10) % 10 + '0');
 		_putchar((n / 1) % 10 + '0');
 	}
-	else if (n <= 9999 && n >= -9999)
+	else if (n <= 9999 || n >= -9999)
 	{
 		_putchar((n / 1000) % 10 + '0');
 		_putchar((n / 100) % 10 + '0');
