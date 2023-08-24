0b261a0579ae62c85178b8f945b8b0eaeec0e86e
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-24 09:06:33 +0000

    6

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n - 1 && src[i] != '\0' ; i++)
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	dest[i + 1] = '\0';
+	for (; i < n ; i++)
+	{
+		dest[i] = '\0';
+	}
 
 	return (dest);
 }
