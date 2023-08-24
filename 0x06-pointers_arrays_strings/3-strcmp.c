a0cd90fcb13706fdbde885ea1848094b9ae46743
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-24 09:33:08 +0000

    6

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,27 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int r;
-
-	r = 0;
-
-	for (i = 0 ; s1[i] != '\0' && s2[i] != '\0' ; i++)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 
-		if (s1[i] == s2[i])
-			break;
-		else if (s1[i] < s2[i])
-		{
-			r += (s1[i] - s2[i]);
-			break;
-		}
-		else if (s2[i] < s1[i])
-		{
-			r -= (s2[i] - s1[i]);
-			break;
-		}
+		s1++;
+		s2++;
 	}
 
-	return (r);
+	return (*s1 - *s2);
 }
