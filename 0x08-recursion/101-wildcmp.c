459774792c1b6e5b3c8777c45d26b05685dec14d
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-30 23:21:21 +0000

    8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,10 +15,18 @@ int wildcmp(char *s1, char *s2)
 		return (*s1 == '\0');
 
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1)/**|| wildcmp(s1 + 1, s2)*/);
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
 
 	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
 		return (wildcmp(s1 + 1, s2 + 1));
+	}
 
 	return (0);
 }
