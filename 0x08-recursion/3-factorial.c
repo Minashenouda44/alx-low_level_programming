06011cd66b03a6829d118f4bf714017442ce2d0b
minashenouda44 <minanadyshenouda@gmail.com>
2023-08-30 19:02:47 +0000

    8

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,11 +10,10 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
+	
+	if (n == 0)
 		return (0);
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+	
+	return (n * factorial(n - 1));
 }
 
