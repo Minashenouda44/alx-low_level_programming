06eef727610515013bf7cf2bd7eb3e6175974c6b
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-04 15:40:13 +0000

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
 
 	p = malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (0);
 
-	int i;
 
 	for (i = 0 ; i < size ; i++)
 	{
