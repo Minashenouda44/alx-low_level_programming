2118a74331b393a69d93a8b7ade89efaf11b15fc
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-05 12:37:49 +0000

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0 ; i < height ; i++)
 	{
-		g[i] = malloc(width * sizeof(int));
+		g[i] = malloc(width * sizeof(int**));
 		if (g[i] == NULL)
 		{
 			for (j = 0 ; j < i ; j++)
