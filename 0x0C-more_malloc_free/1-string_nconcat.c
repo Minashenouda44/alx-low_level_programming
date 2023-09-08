8fab9d5a2d6fa2fd1ef64699ffadadfba9cccf36
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-08 00:58:24 +0000

    ""

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 
 	if (n >= strlen(s2))
 		n = strlen(s2);
