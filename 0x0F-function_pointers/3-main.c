aeb5ecc85871037836f0c44e419931a89fadffaa
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-13 16:06:51 +0000

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * @argc: arg counts
+ * @argv: array of arg values
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+	int (*operation)(int, int);
+
+	if (argc != 4){
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL){
+		printf("Error\n");
+		return (98);
+	}
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%')){
+		printf("Error\n");
+		return (100);
+	}
+
+	result = operation(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
