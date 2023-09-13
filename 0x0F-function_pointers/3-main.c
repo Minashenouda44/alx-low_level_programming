f5033491ec04df313dd0945644d10cc5b872c37c
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-13 16:12:57 +0000

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,24 +14,24 @@ int main(int argc, char *argv[])
 	int result;
 	int (*operation)(int, int);
 
-	if (argc != 4){
+	if (argc != 4)
+	{
 		printf("Error\n");
 		return (98);
 	}
-
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operation = get_op_func(argv[2]);
-
-	if (operation == NULL){
+	if (operation == NULL)
+	{
 		printf("Error\n");
-		return (98);
+		return (99);
 	}
-	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%')){
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
 		printf("Error\n");
 		return (100);
 	}
-
 	result = operation(num1, num2);
 	printf("%d\n", result);
 	return (0);
