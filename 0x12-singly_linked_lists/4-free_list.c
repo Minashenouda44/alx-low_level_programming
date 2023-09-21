d24f4ffa43ffaed0dcc1348887938a4a51adcf42
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-21 20:56:09 +0000

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,18 +8,8 @@
 
 void free_list(list_t *head)
 {
-	list_t *currentNode, *nextNode;
-
 	if (head == NULL)
 		return;
 
-	currentNode = head;
-
-	while (currentNode)
-	{
-		nextNode = currentNode->next;
-		free(currentNode->str);
-		free(currentNode);
-		currentNode = nextNode;
-	}
+	free(head);
 }
