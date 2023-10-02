7e5c02e4d8b3d2efffbab350c8d6e8d98417e97a
minashenouda44 <minanadyshenouda@gmail.com>
2023-10-03 00:02:32 +0300

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - Create a function that creates a file.
+ * @filename: pointer 2 file name
+ * @text_content: content
+ * Return: 1 success -1 failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+    int file_descriptor;
+    int text_length;
+    int text_written;
+
+    if (filename == NULL)
+        return (-1);
+
+    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (file_descriptor == -1)
+        return (-1);
+
+    text_length = strlen(text_content);
+
+    text_written = write(file_descriptor, text_content, text_length);
+
+    if (!text_written || text_written != text_length)
+    {
+        close(file_descriptor);
+        return (-1);
+    }
+
+    close(file_descriptor);
+
+    return (1);
+}
