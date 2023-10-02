15b064f0455996ff8077f1ca5c858c3f2e85df90
minashenouda44 <minanadyshenouda@gmail.com>
2023-10-03 01:42:13 +0300

    22

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,8 @@ int create_file(const char *filename, char *text_content)
 
 	text_length = strlen(text_content);
 
-	text_written = write(file_descriptor, text_content, text_length);
+	if (text_length)
+		text_written = write(file_descriptor, text_content, text_length);
 
 	if (!text_written || text_written != text_length)
 	{
