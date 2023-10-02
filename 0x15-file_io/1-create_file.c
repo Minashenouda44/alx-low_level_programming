df9d98b8cc8c810967892244537a93090c34dcc2
minashenouda44 <minanadyshenouda@gmail.com>
2023-10-03 00:10:18 +0300

    1111

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor;
-    int text_length;
-    int text_written;
+	int file_descriptor;
+	int text_length;
+	int text_written;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (file_descriptor == -1)
-        return (-1);
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+		return (-1);
 
-    text_length = strlen(text_content);
+	text_length = strlen(text_content);
 
-    text_written = write(file_descriptor, text_content, text_length);
+	text_written = write(file_descriptor, text_content, text_length);
 
-    if (!text_written || text_written != text_length)
-    {
-        close(file_descriptor);
-        return (-1);
-    }
+	if (!text_written || text_written != text_length)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
 
-    close(file_descriptor);
+	close(file_descriptor);
 
-    return (1);
+	return (1);
 }
