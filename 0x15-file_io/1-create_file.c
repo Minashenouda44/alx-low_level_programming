db10dba28fce619a6a3cd1539814741cd75e0637
minashenouda44 <minanadyshenouda@gmail.com>
2023-10-03 03:56:05 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,11 +32,11 @@ int create_file(const char *filename, char *text_content)
 		close(file_descriptor);
 		return (-1);
 	}
-	if (text_written != text_length)
+	/*if (text_written != text_length)
 	{
 		close(file_descriptor);
 		return (-1);
-	}
+	}*/
 	close(file_descriptor);
 	return (1);
 }
